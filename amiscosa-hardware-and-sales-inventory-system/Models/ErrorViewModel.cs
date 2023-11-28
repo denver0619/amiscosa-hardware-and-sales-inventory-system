ede29765fe33ea8a7757426666b73cd9c2b06b3c
denver0619 <jdrlaurian@gmail.com>
2023-11-28 20:19:45 +0800

    created project

---
+++ amiscosa-hardware-and-sales-inventory-system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace amiscosa_hardware_and_sales_inventory_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
