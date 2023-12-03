7e72f7f9ac12c908b2c8b04dd19613922c6acade
denver0619 <jdrlaurian@gmail.com>
2023-12-03 20:36:13 +0800

    created User Model

---
+++ amiscosa-hardware-and-sales-inventory-system/Models/IUser.cs
@@ -0,0 +1,13 @@
+﻿namespace amiscosa_hardware_and_sales_inventory_system.Models
+{
+    public interface IUser
+    {
+        public String? UserID { get; set; }
+        public String? FirstName { get; set; }
+        public String? MiddleName { get; set; } 
+        public String? LastName { get; set; }
+        public String? UserName { get; set; }
+        public String? Hash { get; set; }
+        public String? UserRole { get; set; }
+    }
+}
---
+++ amiscosa-hardware-and-sales-inventory-system/Models/User.cs
@@ -0,0 +1,32 @@
+﻿namespace amiscosa_hardware_and_sales_inventory_system.Models
+{
+    public class User : IUser
+    {
+        private string? _userID;
+        private string? _firstName;
+        private string? _middleName;
+        private string? _lastName;
+        private string? _userName;
+        private string? _hash;
+        private string? _userRole;
+
+        User(string userID, string firstName, string middleName, string lastName, string userName, string hash, string userRole)
+        {
+            UserID = userID;
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+            UserName = userName;
+            Hash = hash;
+            UserRole = userRole;
+        }
+
+        public String? UserID { get { return _userID; } set { _userID = value; } }
+        public String? FirstName { get { return _firstName; } set { _firstName = value; } }
+        public String? MiddleName { get { return _middleName; } set { _middleName = value; } }
+        public String? LastName { get { return _lastName; } set { _lastName = value; } }
+        public String? UserName { get { return _userName; } set { _userName = value; } }
+        public String? Hash { get { return _hash; } set { _hash = value; } }
+        public String? UserRole { get { return _userRole; } set { _userRole = value; } }
+    }
+}
