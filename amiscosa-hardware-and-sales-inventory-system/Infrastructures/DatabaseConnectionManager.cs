f6e1bf786e7ab2506bf42e6cf3c985d6c8f79b22
denver0619 <jdrlaurian@gmail.com>
2023-12-03 22:43:00 +0800

    started DatabaseConnectionManager

---
+++ amiscosa-hardware-and-sales-inventory-system/Infrastructures/DatabaseConnectionManager.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace amiscosa_hardware_and_sales_inventory_system.Infrastructures
+{
+    public class DatabaseConnectionManager : IDatabaseConnectionManager
+    {
+        private String _connectionString;
+        private MySqlConnection _connection;
+
+        public string GetConnectionString { get { return _connectionString; } set { _connectionString = value; } }
+        public MySqlConnection GetConnection { get { return _connection; } set { _connection = value; } }
+
+        public void CloseConnection(IDatabaseConnectionManager dbManager)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ amiscosa-hardware-and-sales-inventory-system/Infrastructures/IDatabaseConnectionManager.cs
@@ -0,0 +1,13 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace amiscosa_hardware_and_sales_inventory_system.Infrastructures
+{
+    public interface IDatabaseConnectionManager
+    {
+        public String GetConnectionString { get; set; }
+        public MySqlConnection GetConnection { get; set; }
+
+        public void OpenConnection();
+        public void CloseConnection(IDatabaseConnectionManager dbManager);
+    }
+}
