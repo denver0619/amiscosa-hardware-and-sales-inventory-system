c3b35de403db3252f4b06c9d800118aa509687f6
hanzfernando <fernandohanz23@gmail.com>
2023-12-04 20:31:57 +0800

    made login

---
+++ amiscosa-hardware-and-sales-inventory-system/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using amiscosa_hardware_and_sales_inventory_system.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace amiscosa_hardware_and_sales_inventory_system.Controllers
+{
+    public class AccountController : Controller
+
+    {
+
+
+        public IActionResult Login()
+        {
+            return View("Views/Account/Login.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult Login(String Manager)
+        {
+            bool isManager = !string.IsNullOrEmpty(Manager);
+
+            return RedirectToAction("LoginForm", "Account", new { isManager });
+        }
+
+        public IActionResult LoginForm(bool isManager)
+        {
+            ViewData["isManager"] = isManager;
+            return View("Views/Account/LoginForm.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult LoginForm(LoginFormModel model)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ amiscosa-hardware-and-sales-inventory-system/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿namespace amiscosa_hardware_and_sales_inventory_system.Models
+{
+
+    public class LoginModel
+    {
+        // assign role if user is manager or not
+    }
+    public class LoginFormModel
+    {
+        //public string Username { get; set; }
+        //public string Password { get; set; }
+        // Add other necessary fields for login
+    }
+}
