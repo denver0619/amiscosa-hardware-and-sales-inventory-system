b356e09923f37709dbd7f80f361f731bbb58b322
denver0619 <jdrlaurian@gmail.com>
2023-12-17 22:47:39 +0800

    finished Transaction model

---
+++ amiscosa-hardware-and-sales-inventory-system/Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace amiscosa_hardware_and_sales_inventory_system.Models
+{
+    public class Transaction : ITransaction
+    {
+        public string TransactionID { get; set; } = null!;
+        public string TransactionDate { get; set; } = null!;
+        public string StaffID { get; set; } = null!;
+        public string CustomerID { get; set; } = null!;
+        public bool IsInvalid { get; set; }
+    }
+}
