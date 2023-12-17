e8ab6f4e8767aea14bdb42f634be2d939385b2eb
denver0619 <jdrlaurian@gmail.com>
2023-12-17 22:57:40 +0800

    modified Transaction model

--- amiscosa-hardware-and-sales-inventory-system/Models/ITransaction.cs
+++ amiscosa-hardware-and-sales-inventory-system/Models/ITransaction.cs
@@ -4,10 +4,10 @@ namespace amiscosa_hardware_and_sales_inventory_system.Models
 {
     public interface ITransaction
     {
-        public string TransactionID { get; set; }
-        public string TransactionDate { get; set; }
-        public string StaffID { get; set; }
-        public string CustomerID { get; set; }
+        public string? TransactionID { get; set; }
+        public string? TransactionDate { get; set; }
+        public string? StaffID { get; set; }
+        public string? CustomerID { get; set; }
         public bool IsInvalid { get; set; }
     }
 }
--- amiscosa-hardware-and-sales-inventory-system/Models/Transaction.cs
+++ amiscosa-hardware-and-sales-inventory-system/Models/Transaction.cs
@@ -1,11 +1,18 @@
-﻿namespace amiscosa_hardware_and_sales_inventory_system.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace amiscosa_hardware_and_sales_inventory_system.Models
 {
     public class Transaction : ITransaction
     {
-        public string TransactionID { get; set; } = null!;
-        public string TransactionDate { get; set; } = null!;
-        public string StaffID { get; set; } = null!;
-        public string CustomerID { get; set; } = null!;
+        [Required]
+        public string? TransactionID { get; set; }
+        [Required]
+        public string? TransactionDate { get; set; }
+        [Required]
+        public string? StaffID { get; set; }
+        [Required]
+        public string? CustomerID { get; set; }
+        [Required]
         public bool IsInvalid { get; set; }
     }
 }
