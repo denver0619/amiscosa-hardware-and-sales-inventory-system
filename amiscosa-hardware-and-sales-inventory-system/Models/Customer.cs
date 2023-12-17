528392cf07ea9b3f596fac16e96d6adb03ffbf72
denver0619 <jdrlaurian@gmail.com>
2023-12-17 23:13:13 +0800

    Modified Customer model

--- amiscosa-hardware-and-sales-inventory-system/Models/Customer.cs
+++ amiscosa-hardware-and-sales-inventory-system/Models/Customer.cs
@@ -4,6 +4,15 @@ namespace amiscosa_hardware_and_sales_inventory_system.Models
 {
     public class Customer : ICustomer
     {
+        public Customer(string customerID, string firstName, string middleName, string lastName, string address, string contact)
+        {
+            CustomerID = customerID;
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+            Address = address;
+            Contact = contact;
+        }
         [Required]
         public string? CustomerID { get; set; }
         [Required]
