32a6de1ec19f4dfc3a1417c4e94e7e2c0053fb2f
denver0619 <jdrlaurian@gmail.com>
2023-12-17 23:19:21 +0800

    finished Product model

---
+++ amiscosa-hardware-and-sales-inventory-system/Models/IProduct.cs
@@ -0,0 +1,15 @@
+﻿namespace amiscosa_hardware_and_sales_inventory_system.Models
+{
+    public interface IProduct
+    {
+        public string? ProductID { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public string? ManufacturerID { get; set; }
+        public string? Measurement {  get; set; }
+        public bool IsAvailable { get; set; }
+        public int UnitCost { get; set; }
+    }
+}
---
+++ amiscosa-hardware-and-sales-inventory-system/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Formats.Asn1;
+
+namespace amiscosa_hardware_and_sales_inventory_system.Models
+{
+    public class Product : IProduct
+    {
+        public Product(string productID, string productName, string productDescription, int unitPrice,int quantity, string manufacturerID, string measurement, bool isAvailable, int unitCost)
+        {
+            ProductID = productID;
+            ProductName = productName;
+            ProductDescription = productDescription;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
+            ManufacturerID = manufacturerID;
+            Measurement = measurement;
+            IsAvailable = isAvailable;
+            UnitCost = unitCost;
+        }
+        [Required]
+        public string? ProductID { get; set; }
+        [Required]
+        public string? ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+        [Required]
+        public int UnitPrice { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public string? ManufacturerID { get; set; }
+        public string? Measurement { get; set; }
+        [Required]
+        public bool IsAvailable { get; set; }
+        [Required]
+        public int UnitCost { get; set; }
+    }
+}
