e7d0cb45b66f6be007d28bb0b76932aaa3cca0ae
denver0619 <jdrlaurian@gmail.com>
2023-12-27 01:10:32 +0800

    started ProductRepository

---
+++ amiscosa-hardware-and-sales-inventory-system/Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using amiscosa_hardware_and_sales_inventory_system.Domain.Entities;
+
+namespace amiscosa_hardware_and_sales_inventory_system.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        void AddProduct(ProductRepository product);
+        void DeleteProduct(ProductRepository product);
+        void EditProduct(ProductRepository product);
+        ProductRepository GetProductByID(string id);
+        ProductRepository GetProductByName(string name);
+        List<ProductRepository> GetProducts();
+    }
+}
---
+++ amiscosa-hardware-and-sales-inventory-system/Domain/Repositories/ProductRepository.cs
@@ -0,0 +1,51 @@
+﻿
+namespace amiscosa_hardware_and_sales_inventory_system.Domain.Repositories
+{
+    public class ProductRepository : IProductRepository, IDisposable, IInitializable
+    {
+
+        public ProductRepository()
+        {
+
+        }
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddProduct(ProductRepository product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(ProductRepository product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditProduct(ProductRepository product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductRepository GetProductByID(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductRepository GetProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductRepository> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
