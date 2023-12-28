5f8d1fed686b9736c75f8590ac93ce1ef0b55fe3
denver0619 <jdrlaurian@gmail.com>
2023-12-28 19:03:57 +0800

    Finished Notification Entity

---
+++ amiscosa-hardware-and-sales-inventory-system/Domain/Entities/INotification.cs
@@ -0,0 +1,11 @@
+﻿namespace amiscosa_hardware_and_sales_inventory_system.Domain.Entities
+{
+    public interface INotification
+    {
+        public string? NotificationID { get; set; }
+        public string? NotificationName { get; set; }
+        public string? NotificationDescription { get; set; }
+        public string? ProductID { get; set; }
+        public int NotificationType { get; set; }
+    }
+}
