ffd689e49af39386e0ca54d8029b89683642fb3b
ggiyang <apadjaeiamikaella@gmail.com>
2023-12-29 23:50:53 +0800

    created NotificationsRepository

---
+++ amiscosa-hardware-and-sales-inventory-system/Domain/Repositories/NotificationRepository.cs
@@ -0,0 +1,41 @@
+﻿using amiscosa_hardware_and_sales_inventory_system.Domain.Entities;
+using amiscosa_hardware_and_sales_inventory_system.Infrastructures;
+using System.Data;
+
+namespace amiscosa_hardware_and_sales_inventory_system.Domain.Repositories
+{
+    public class NotificationRepository : INotificationRepository
+    {
+        private DatabaseHelper<Notification> databaseHelper;
+        private readonly string tableName = "notifications";
+        
+
+        public NotificationRepository()
+        {
+            databaseHelper = new DatabaseHelper<Notification>();
+        }
+
+        
+        public void AddNotification(INotification notification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Notification> GetAllNotification()
+        {
+            DataTable dataTable = databaseHelper.SelectAllRecord(tableName);
+            List<Notification> notifications = new List<Notification>();
+            foreach (DataRow row in dataTable.Rows)//retrieve data per row
+            {
+                Notification notification = new Notification(row["NotificationID"].ToString()!, row["NotificationName"].ToString()!, row["NotificationDescription"].ToString()!, row["ProductID"].ToString()!, Int32.Parse (row["NotificationType"].ToString()!));
+                notifications.Add(notification);
+            }
+            return notifications;
+        }
+
+        public Notification GetNotificationByID(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
