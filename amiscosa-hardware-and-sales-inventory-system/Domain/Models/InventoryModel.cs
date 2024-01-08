0760a5c0ba9b20272e32be16e8724f9c5efe204f
denver0619 <jdrlaurian@gmail.com>
2024-01-08 11:28:38 +0800

    created InventoryModel and ProductService

---
+++ amiscosa-hardware-and-sales-inventory-system/Domain/Models/InventoryModel.cs
@@ -0,0 +1,13 @@
+﻿using amiscosa_hardware_and_sales_inventory_system.Domain.Entities;
+
+namespace amiscosa_hardware_and_sales_inventory_system.Domain.Models
+{
+    public class InventoryModel
+    {
+        public InventoryModel(List<Product> productList)
+        {
+            ProductList = productList;
+        }
+        public List<Product> ProductList { get; set; }
+    }
+}
---
+++ amiscosa-hardware-and-sales-inventory-system/Services/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using amiscosa_hardware_and_sales_inventory_system.Domain.Entities;
+using amiscosa_hardware_and_sales_inventory_system.Domain.Models;
+using amiscosa_hardware_and_sales_inventory_system.Domain.Repositories;
+
+namespace amiscosa_hardware_and_sales_inventory_system.Services
+{
+    public class ProductService : IDisposable
+    {
+        private InventoryModel inventoryModel;
+        private ProductRepository productRepository;
+
+        public ProductService()
+        {
+            productRepository = new ProductRepository();
+            inventoryModel = new InventoryModel(GetProductList());
+        }
+
+        public InventoryModel GetModel()
+        {
+            return inventoryModel;
+        }
+
+        public List<Product> GetProductList()
+        {
+            return productRepository.GetAllProducts();
+        }
+
+        public void Dispose()
+        {
+            productRepository.Dispose();
+        }
+
+    }
+}
