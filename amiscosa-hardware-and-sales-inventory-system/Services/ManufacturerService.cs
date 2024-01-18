5c0deb0cfc0a1e2878062492efd7e396152bf8ef
denver0619 <jdrlaurian@gmail.com>
2024-01-18 12:20:40 +0800

    created manufacturer model and service

---
+++ amiscosa-hardware-and-sales-inventory-system/Services/ManufacturerService.cs
@@ -0,0 +1,41 @@
+﻿using amiscosa_hardware_and_sales_inventory_system.Domain.Entities;
+using amiscosa_hardware_and_sales_inventory_system.Domain.Models;
+using amiscosa_hardware_and_sales_inventory_system.Domain.Repositories;
+
+namespace amiscosa_hardware_and_sales_inventory_system.Services
+{
+    public class ManufacturerService : IDisposable
+    {
+        private ManufacturerRepository manufacturerRepository;
+
+        public ManufacturerService()
+        {
+            manufacturerRepository = new ManufacturerRepository();
+            Model = new ManufacturerModel();
+            Model = GetAllManufacturers();
+        }
+
+        public ManufacturerModel Model { get; set; }
+
+        public ManufacturerModel GetAllManufacturers()
+        {
+            Model.ManufacturerList = manufacturerRepository.GetAllManufacturers();
+            return Model;
+        }
+
+        public void AddManufacturer(Manufacturer manufacturer)
+        {
+            manufacturerRepository.AddManufacturer(manufacturer);
+        }
+
+        public void EditManufacturer(Manufacturer manufacturer)
+        {
+            manufacturerRepository.UpdateManufacturer(manufacturer);
+        }
+
+        public void Dispose()
+        {
+            manufacturerRepository.Dispose();
+        }
+    }
+}
