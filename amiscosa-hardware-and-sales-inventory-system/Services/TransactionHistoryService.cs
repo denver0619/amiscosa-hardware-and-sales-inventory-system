d21c69f661d4cfa9250ecbbba1f965c3847642ca
denver0619 <jdrlaurian@gmail.com>
2024-01-18 15:59:38 +0800

    created transaction history model

---
+++ amiscosa-hardware-and-sales-inventory-system/Services/TransactionHistoryService.cs
@@ -0,0 +1,29 @@
+﻿using amiscosa_hardware_and_sales_inventory_system.Domain.Models;
+using amiscosa_hardware_and_sales_inventory_system.Domain.Repositories;
+
+namespace amiscosa_hardware_and_sales_inventory_system.Services
+{
+    public class TransactionHistoryService : IDisposable
+    {
+        TransactionRepository transactionRepository;
+        public TransactionHistoryService()
+        {
+            transactionRepository = new TransactionRepository();
+            Model = new TransactionHistoryModel();
+            Model = GetAllTransaction();
+        }
+        public TransactionHistoryModel Model { get; set; }
+
+        public TransactionHistoryModel GetAllTransaction()
+        {
+            Model.TransactionList = transactionRepository.GetAllTransactions().OrderByDescending(transaction=>transaction.TransactionID).ToList();
+            return Model;
+        }
+
+        public void Dispose()
+        {
+            transactionRepository.Dispose();
+        }
+
+    }
+}
